2c1c39b99ac2e9450e608427e4a91e747be70133
MK <pminhkhue1308@gmail.com>
2024-04-15 16:19:40 +0200

    reorganise files

--- src/main/IGrapheConst.java
+++ src/main/IGrapheConst.java
@@ -10,6 +10,8 @@ public interface IGrapheConst {
 	List<String> getSucc(String sommet); // pas forcement triee
 	int getValuation(String src, String dest); // les sommets doivent exister, -1 si pas d'arc
 	boolean contientSommet(String sommet);
+
+
 	boolean contientArc(String src, String dest);
 	
 	default String toAString() {
